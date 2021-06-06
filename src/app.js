5300637b644ed090da5edcf0e730d5ba09ca7ae0
https://learn.co/arf2k <foreman.alexander@gmail.com>
2021-06-06 18:02:37 -0400

    Express server set up with several routes

--- src/app.js
+++ src/app.js
@@ -1,1 +1,27 @@
 const express = require("express")
+
+const app = express() 
+
+app.get('', (req, res ) => {
+res.send("Sup")
+})
+
+//get method takes route (partial url) and function describing what you want to do when they visit it 
+//send - reply 
+
+app.get("/help", (req, res) => {
+     res.send("Help me!!!")
+})
+
+app.get("/about", (req, res) => {
+     res.send("Check out this about page!")
+})
+
+app.get("/weather", (req, res) => {
+     res.send("Weatherpage")
+})
+
+
+app.listen(3000, () => {
+     console.log("Server up on port 3000")
+})
